75ba4c5f21c15e66e64d5d0ce4b8703cf070c45e
21120064 <lequochan2003@gmail.com>
2024-05-26 23:19:25 +0700

    PaginationUI

---
+++ front-end/src/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row my-3">
+    <v-pagination
+      :model-value="currentPage"
+      :pages="totalPages"
+      :range-size="1"
+      active-color="#DCEDFF"
+      @update:modelValue="handleChangePage"
+    />
+  </div>
+</template>
+
+<script>
+import VPagination from "@hennge/vue3-pagination";
+import "@hennge/vue3-pagination/dist/vue3-pagination.css";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "PaginationComponent",
+  components: {
+    VPagination,
+  },
+  computed: {
+    ...mapState({
+      currentPage: (state) => state.currrentPage,
+      totalPages: (state) => state.totalPages,
+    }),
+  },
+  methods: {
+    ...mapActions(["updateCurrentPage"]),
+    handleChangePage(newPage) {
+      this.updateCurrentPage(newPage);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.Pagination {
+  justify-content: center;
+}
+</style>
