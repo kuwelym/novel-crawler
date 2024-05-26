7e9141234202777a1348b27ec9c60598ef869a66
kuwelym <klyminh@gmail.com>
2024-05-26 10:56:21 +0700

    initialize project structure with backend and frontend directories

---
+++ back-end/src/test/java/com/group21/novel_crawler/NovelCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group21.novel_crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
